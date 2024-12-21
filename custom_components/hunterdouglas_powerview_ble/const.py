e8693f126f08388f5543b9431eb7348c201feafe
patman15 <14628713+patman15@users.noreply.github.com>
2024-12-21 09:10:40 +0100

    stronger typing

--- custom_components/hunterdouglas_powerview_ble/const.py
+++ custom_components/hunterdouglas_powerview_ble/const.py
@@ -13,12 +13,15 @@ from typing import Final
 # )
 
 
-DOMAIN: Final = "hunterdouglas_powerview_ble"
+DOMAIN: Final[str] = "hunterdouglas_powerview_ble"
 LOGGER: Final = logging.getLogger(__package__)
-MFCT_ID: Final = 2073
-TIMEOUT: Final = 5
-HOME_KEY: Final = b""
+MFCT_ID: Final[int] = 2073
+TIMEOUT: Final[int] = 5
+
+# put the key here, needs to be 16 bytes long, e.g.
+# HOME_KEY: Final[bytes] = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+HOME_KEY: Final[bytes] = b""
 
 
 # attributes (do not change)
-ATTR_RSSI = "rssi"
+ATTR_RSSI: Final[str] = "rssi"
